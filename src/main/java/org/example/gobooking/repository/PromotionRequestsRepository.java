3a91cb7583a666014ee8d1e06c929e983763bf0e
Gevorg_Qocharyan <gkocharyan347@gmail.com>
2025-01-31 00:04:02 +0400

    Changed method name

--- src/main/java/org/example/gobooking/repository/PromotionRequestsRepository.java
+++ src/main/java/org/example/gobooking/repository/PromotionRequestsRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PromotionRequestsRepository extends JpaRepository<PromotionRequest, Integer> {
 
-    PromotionRequest findPromotionRequestByRequester_id(Integer requester_id);
+    PromotionRequest findPromotionRequestByRequesterId(int requesterId);
 
 }
