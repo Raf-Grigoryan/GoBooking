bca771b79da52602aa6982e3515998c050bb6713
Raf-Grigoryan <raf.grigoryan18@mail.ru>
2025-02-06 15:43:20 +0400

    Implemented User Panel with User Information management, including profile editing, password change, image upload, and other updates. Added Card Deletion functionality and Finance Overview, displaying total balance and all linked cards.

--- src/main/java/org/example/gobooking/mapper/CompanyMapper.java
+++ src/main/java/org/example/gobooking/mapper/CompanyMapper.java
@@ -3,7 +3,6 @@ package org.example.gobooking.mapper;
 import org.example.gobooking.dto.company.CompanyDto;
 import org.example.gobooking.dto.company.SaveCompanyRequest;
 import org.example.gobooking.entity.company.Company;
-import org.example.gobooking.service.AddressService;
 import org.example.gobooking.service.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
